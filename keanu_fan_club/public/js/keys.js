675ce12f3db9e65713d1ff151a613ac3b5adc2ec
Joe Keohah <jkeohan@gmail.com>
2018-11-12 17:10:26 -0500

    updates

---
+++ keanu_fan_club/public/js/keys.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAbrfm5sXCumEPFzUBYNcrNYfRG55pwwoA",
+    authDomain: "message-app-test.firebaseapp.com",
+    databaseURL: "https://message-app-test.firebaseio.com",
+    projectId: "message-app-test",
+    storageBucket: "message-app-test.appspot.com",
+    messagingSenderId: "418802423823"
+  };
+  firebase.initializeApp(config);
---
+++ korn_app_starter_code_copy/public/js/keys.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAbrfm5sXCumEPFzUBYNcrNYfRG55pwwoA",
+    authDomain: "message-app-test.firebaseapp.com",
+    databaseURL: "https://message-app-test.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "418802423823"
+  };
+  firebase.initializeApp(config);
