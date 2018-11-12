91ac84fa68118cbfa94a458b23bab5f857909546
Joe Keohan <joekeohan@Joes-MacBook-Pro-2.local>
2018-11-12 18:05:20 -0500

    initial

--- korn_app_starter_code_copy/public/js/keys.js
+++
@@ -1,9 +0,0 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAbrfm5sXCumEPFzUBYNcrNYfRG55pwwoA",
-    authDomain: "message-app-test.firebaseapp.com",
-    databaseURL: "https://message-app-test.firebaseio.com",
-    storageBucket: "",
-    messagingSenderId: "418802423823"
-  };
-  firebase.initializeApp(config);
